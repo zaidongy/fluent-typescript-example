4bdc4263dea68e6bd8928102ccb3900f5ab51c65
patrick.wilson <patrick.wilson@servicenow.com>
2024-08-16 11:29:16 -0700

    Add a gs info message

--- src/server/script.ts
+++ src/server/script.ts
@@ -4,5 +4,6 @@ export function showStateUpdate(current:any, previous:any) {
     const currentState = current.getValue('state')
     const previousState = previous.getValue('state')
 
+    gs.addInfoMessage('Hello from Typescript!')
     gs.addInfoMessage(`state updated from "${previousState}" to "${currentState}"`)
 }
